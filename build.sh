de37ecdae7d5e0d484966377e20fb2b94301a0bf
FireDroX <FireDroX@gmail.com>
2026-04-25 16:08:52 +0200

    fix: bind container port to localhost for improved security

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DOCKER_BUILDKIT=1 docker build -t portfolio-image .
 
 echo "🚀 Lancement container..."
 docker run -d \
-  -p 3000:3000 \
+  -p 127.0.0.1:3000:3000 \
   --name portfolio \
   -v portfolio_projects:/app/projects \
   portfolio-image:latest
