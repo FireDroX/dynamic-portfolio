a8ea98387433e2a0e212ca1cb119d4a65efcf641
FireDroX <FireDroX@gmail.com>
2026-05-04 10:00:45 +0200

    feat: reorganize build script for improved clarity and efficiency

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+echo "📦 Build image..."
+DOCKER_BUILDKIT=1 docker build -t portfolio-image .
+
 echo "🛑 Stop container si existant..."
 docker stop portfolio 2>/dev/null
 
 echo "🗑️ Suppression container..."
 docker rm portfolio 2>/dev/null
 
-echo "📦 Build image..."
-DOCKER_BUILDKIT=1 docker build -t portfolio-image .
-
 echo "🚀 Lancement container..."
 docker run -d \
   -p 127.0.0.1:3000:3000 \
   --name portfolio \
+  --restart unless-stopped \
   -v portfolio_projects:/app/projects \
   portfolio-image:latest
 
